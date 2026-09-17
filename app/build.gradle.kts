acb309cd669054f706b5c97dad5fb99e2d547ddd
ran4erep <45404569+ran4erep@users.noreply.github.com>
2026-09-18 01:45:53 +0300

    Last Sync: 2026-09-18 01:45 (Mobile)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,11 +15,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.ran4erep.r4ezka.wxmpuo"
+    applicationId = "com.ran4erep.r4ezka"
     minSdk = 24
     targetSdk = 36
     versionCode = 4
-    versionName = "1.0.4"
+    versionName = "1.0.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
